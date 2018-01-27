e69d181a2a46318204660eadb872f473b8a445de
Andres Sedano <andysedano@gmail.com>
2018-01-27 00:59:03 -0600

    NPC, GameManager
    Funcionalidad basica de los npc e inicio del game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public int player1Score = 0;
+	public int player2Score = 0;
+	public float timeLeft;
+
+	private bool inGame = false;
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inGame){
+			timeLeft -= Time.deltaTime;
+			if(timeLeft < 0){
+				endGame ();
+			}
+		}
+	}
+
+	//Empezar el juego
+	void beginGame(){
+		SceneManager.LoadScene ("Game");
+		timeLeft = 30;
+		loadNPCS ();
+	}
+
+	//Cargar el PreJuego
+	void loadPreGame(){
+		SceneManager.LoadScene ("PreGame");
+	}
+		
+	//Se muere todo
+	void endGame(){
+		
+	}
+
+	void loadNPCS(){
+		
+	}
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	private int estado;	//0-Normal, 1-Poseido
+	private int trait;
+
+	// Use this for initialization
+	void Start () {
+		estado = 0;
+		trait = Random.Range (0,1);
+		transform.Rotate (Vector3.up, Random.Range (0f ,360f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * 0.3f);
+	}
+
+	//Para chocar con otros NPCS
+	private void OnTriggerEnter(Collider hit){
+		if(hit.tag.Equals("NPC")){
+			//cambiar direccion
+		} else if(hit.tag.Equals("Player")) {
+			//Hacer cosas de player
+		}
+	}
+
+	//Para detectar si choco con el player
+	private void OnCollisionEnter(Collision hit){
+
+	}
+}
