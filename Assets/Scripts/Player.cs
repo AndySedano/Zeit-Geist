c130f0c92247cb86c20e7adbaf2392db4f7422d3
Os009 <oszilla009@gmail.com>
2018-01-27 00:59:46 -0600

    Player and PlayerController implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool isPlayer1;
+
+    //speed variable
+    public float movespeed = 5f;
+
+    //
+    Vector3 moveVelocity;
+
+    PlayerController controller;
+
+    //movement variables
+    float player1x;
+    float player1y;
+    float player2x;
+    float player2y;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+
+    void Update()
+    {
+
+        if (isPlayer1)
+        {
+            moveVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        else
+        {
+            moveVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+
+        //move the player
+        
+        controller.Move(moveVelocity * movespeed);
+        
+
+        
+        
+    }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    float angle;
+
+    Vector3 velocity;
+
+    Rigidbody rigidBody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        rigidBody.MovePosition(velocity * Time.fixedDeltaTime + rigidBody.position);
+    }
+
+    
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+        float quickangle = (Mathf.Atan2(velocity.x, velocity.z) * Mathf.Rad2Deg) - 180;
+        if (velocity.x!=0f || velocity.z!=0f)
+        {
+            angle = quickangle;
+        }
+        transform.rotation = Quaternion.Euler(0f, angle, 0f) ;
+    }
+}
